5755a2e72733f7bf4cad15f245a2f8528d773856
Javier Sanchez <jsanchez@psdc.com.pa>
2021-01-26 07:45:45 -0500

    unique

---
+++ pw_unique_product_code/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Unique Product Code (Internal Reference)",
+    'summary': """
+        This module is allow to block saving of product with duplicate code that is currently used by another product.""",
+    'description': """
+        This module is allow to block saving of product with duplicate code that is currently used by another product.
+- Product Internal Reference Unique
+- Product Code Unique
+- Unique Product Internal Reference
+- Unique Default Code
+        """,    
+    'version': '13.0',
+    'author': "Preway IT Solutions",
+    'category': 'Extra Tools',
+    'depends': ['product'],
+    'images':[
+        'static/description/banner.png',
+    ],
+    'price': 8.0,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    "images":["static/description/Banner.png"],
+}
---
+++ pw_unique_product_code/models/product.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.constrains('default_code')
+    def _check_duplicate_code(self):
+        res = self.search([('default_code', '=', self.default_code), ('default_code', '!=', False)])
+        if len(res) > 1:
+            raise ValidationError(_('Internal Reference must be unique !'))
