fba467bde19e5185b07602ac2ce1215a5dd326e0
Joel Gutierrez <40271253+joelega@users.noreply.github.com>
2021-02-11 11:25:45 -0500

    Update 2

--- product_sale_stock_warning/models/product.py
+++ product_sale_stock_warning/models/product.py
@@ -8,15 +8,7 @@ class Users(models.Model):
 
     check_no_stock = fields.Boolean(
     	string="Venta Sin Stock",
-        help="Permite ver los check para hacer ventas sin stock"
-    )
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    custom_check_onhand_qty = fields.Boolean(
-    	string="Sales Order Alert",
-        help="If qty not in hand at warehouse then raise alert on confirm sales time .",
+        help="Permite ver los check para hacer ventas sin stock",
         compute="_compute_hide"
     )
 
@@ -25,7 +17,15 @@ class ProductTemplate(models.Model):
     )
 
     def _compute_hide(self):
-        if self.env['res.users'].search([('check_no_stock','=',True)]):
+        if self.check_no_stock == True:
             self.hide = True
         else:
             self.hide = False
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    custom_check_onhand_qty = fields.Boolean(
+    	string="Sales Order Alert",
+        help="If qty not in hand at warehouse then raise alert on confirm sales time ."
+    )
