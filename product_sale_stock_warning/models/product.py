d506018a875418ebd1725dd4fdcf38c39640b8d9
Joel Gutierrez <40271253+joelega@users.noreply.github.com>
2021-02-17 09:17:51 -0500

    update product sale warning

---
+++ product_sale_stock_warning/models/product.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+
+class Users(models.Model):
+    _inherit = 'res.users'
+
+    check_no_stock = fields.Boolean(
+    	string="Venta Sin Stock",
+        help="Permite ver los check para hacer ventas sin stock",
+        readonly=False
+    )
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    custom_check_onhand_qty = fields.Boolean(
+    	string="Sales Order Alert",
+        help="If qty not in hand at warehouse then raise alert on confirm sales time ."
+    )
+
+    hide = fields.Boolean(
+        string="Oculto",
+        compute="_compute_hide"
+    )
+
+    def _compute_hide(self):
+        if self.env['res.users'].search([('check_no_stock', '=', True)]):
+            self.hide = True
+        else:
+            self.hide = False
